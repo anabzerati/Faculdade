4dbfce18bef2071bc01efa15808c750196267d12
Ana Beatriz Zerati <anabeatrizsilvazerati28@gmail.com>
2023-01-31 21:41:11 -0300

    Create exercicio 1.c

---
+++ Primeiro período/lista3/exercicio 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int numero;
+
+  printf("Insira um número inteiro: ");
+  scanf("%d", &numero);
+
+  if(numero<20||numero>100){
+    printf("Número fora do intervalo estipulado");
+  } 
+
+    return 0;
+}
